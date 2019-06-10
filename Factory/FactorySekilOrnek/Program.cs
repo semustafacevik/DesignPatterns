947ca531a92ae8830139c8fe0b952f12efab51e5
Mustafa CEVIK <39580114+semustafacevik@users.noreply.github.com>
2019-06-11 01:41:55 +0300

    Add Factory files

---
+++ Factory/FactorySekilOrnek/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorySekilOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SekilFactory sf = new SekilFactory();
+
+            ISekil sekil1 = sf.GetSekil(SekilFactory.enSekilTuru.enKare);
+            sekil1.SekilCiz();
+
+            ISekil sekil2 = sf.GetSekil(SekilFactory.enSekilTuru.enDaire);
+            sekil2.SekilCiz();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Factory/FactorySekilOrnek/SekilFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorySekilOrnek
+{
+    public class SekilFactory
+    {
+        public enum enSekilTuru
+        {
+            enKare,
+            enDaire,
+        };
+
+        public ISekil GetSekil(enSekilTuru sekilTuru)
+        {
+            switch (sekilTuru)
+            {
+                case enSekilTuru.enKare:
+                    return new Kare();
+
+                case enSekilTuru.enDaire:
+                    return new Daire();
+
+                default:
+                    return null;
+            }
+        }
+    }
+}

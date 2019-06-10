39ce5db44c2f92ad8a108da6f4a0c7170566753b
Mustafa CEVIK <39580114+semustafacevik@users.noreply.github.com>
2019-06-11 01:48:32 +0300

    Add Singleton files.

---
+++ Singleton/SingletonOrnek/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleObject s1 = SingleObject.getIntance();
+            SingleObject s2 = SingleObject.getIntance();
+            SingleObject s3 = SingleObject.getIntance();
+        }
+    }
+}
---
+++ Singleton/SingletonOrnek/SingleObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonOrnek
+{
+    public class SingleObject
+    {
+        private static SingleObject instance;
+
+        private SingleObject()
+        {
+        }
+
+        public static SingleObject getIntance()
+        {
+            if (instance == null)
+            {
+                instance = new SingleObject();
+                Console.WriteLine("Yeni");
+            }
+
+            else
+                Console.WriteLine("Var olan");
+
+            return instance;
+        }
+
+    }
+}
